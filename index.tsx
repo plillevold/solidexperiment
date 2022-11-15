515ef28fa34016a6a621dc3cd8bb5618c0e8858a
Peter <plillevold@users.noreply.github.com>
2022-11-15 21:38:56 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,8 +2,8 @@ import { createSignal, onCleanup, Component } from "solid-js";
 import { render } from "solid-js/web";
 
 const App: Component = () => {
-  const [count, setCount] = createSignal(0),
-    timer = setInterval(() => setCount((c) => c + 1), 1000);
+  var [count, setCount] = createSignal(0);
+  var timer = setInterval(() => setCount((c) => c + 1), 1000);
   onCleanup(() => clearInterval(timer));
 
   return <div>{count()}</div>;
